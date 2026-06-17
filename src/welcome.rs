37e594ff9f7874a5ee7d2f38ba4a05ca1a3423f7
mjv-7 <mujibullafikrat@gmail.com>
2026-06-17 12:38:48 -0400

    update10

--- src/welcome.rs
+++ src/welcome.rs
@@ -4,36 +4,99 @@ Date: 2026-04-08
 Screen Details:
  */
 
-use macroquad::prelude::*;
+use crate::modules::grid::draw_grid;
 use crate::modules::label::Label;
-use crate::modules::text_button::TextButton;
 use crate::modules::scale::use_virtual_resolution;
-use crate::modules::grid::{draw_grid};
-pub async fn run() -> String{
-        let lbl_out = Label::new("Hello To the Space Invaders
-Your Objective is to Defeat the Aliens", 50.0, 100.0, 30);
-        let btn_next = TextButton::new(
-        620.0,
-        200.0,
-        200.0,
-        60.0,
-        "Click to Play!",
-        BLUE,
-        GREEN,
-        30
+use crate::modules::still_image::StillImage;
+use crate::modules::text_button::TextButton;
+use macroquad::prelude::*;
+pub async fn run() -> String {
+    let mut img_ship = StillImage::new(
+        "assets/player.png",
+        100.0,  // width
+        250.0,  // height
+        1070.0, // x (will be set in the loop)
+        200.0,  // y position
+        true,
+        1.0,
+    )
+    .await;
+    let img_enemy = StillImage::new(
+        "assets/enemy.png",
+        150.0,  // width
+        150.0,  // height
+        340.0, // x (will be set in the loop)
+        540.0,  // y position
+        true,
+        1.0,
+    )
+    .await;
+    let mut img_bullet = StillImage::new(
+        "assets/bullet.png",
+        35.0,  // width
+        35.0,  // height
+        960.0, // x (will be set in the loop)
+        380.0,  // y position
+        true,
+        1.0,
+    ).await;
+    let mut img_bullet2 = StillImage::new(
+        "assets/bullet.png",
+        35.0,  // width
+        35.0,  // height
+        480.0, // x (will be set in the loop)
+        520.0,  // y position
+        true,
+        1.0,
+    ).await;
+    img_bullet.set_angle(70.0);
+    img_bullet2.set_angle(-90.0);
+    img_ship.set_angle(-90.0);
+    let mut lbl_out = Label::new(
+        "Hello To the Space Invaders
+Your Objective is to Defeat the Aliens",
+        50.0,
+        100.0,
+        30,
     );
-    let lbl_directions = Label::new("A and D keys are for movement,
+    lbl_out.with_colors(WHITE, Some(DARKGRAY));
+    let btn_next = TextButton::new(640.0, 480.0, 200.0, 60.0, "Click to Play!", BLUE, GREEN, 30);
+    let mut lbl_directions = Label::new(
+        "A and D keys are for movement,
     your main objective is to destroy the Aliens,
-    and not loose your three hearts", 50.0, 800.0, 50);
+    and not loose your three hearts",
+        50.0,
+        800.0,
+        50,
+    );
+    lbl_directions.with_colors(WHITE, Some(DARKGRAY));
+    let img_bg = StillImage::new(
+        "assets/bg.png",
+        1440.0, // width
+        1080.0, // height
+        0.0,    // x (will be set in the loop)
+        0.0,    // y position
+        true,
+        1.0,
+    )
+    .await;
     loop {
         use_virtual_resolution(1440.0, 1080.0);
-        clear_background(RED);
+        clear_background(BLUE);
+        
+        
+        
+        img_bg.draw();
+        img_ship.draw();
+        img_enemy.draw();
+        lbl_out.draw();
+        lbl_directions.draw();
+        img_bullet.draw();
+        img_bullet2.draw();
         if btn_next.click() {
             return "game".to_string();
         }
-        lbl_out.draw();
-        lbl_directions.draw();
-        draw_grid(40.0, BLACK);
+         draw_grid(40.0, WHITE);
         next_frame().await;
     }
 }
